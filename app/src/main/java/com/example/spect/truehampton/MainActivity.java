c6d16bf809786b8d3193b0898e2ded2968584bb5
Luis945 <luis_aguirre_1994@outlook.com>
2018-04-25 22:08:44 -0500

    estoy en eso homs

--- app/src/main/java/com/example/spect/truehampton/MainActivity.java
+++ app/src/main/java/com/example/spect/truehampton/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.spect.truehampton;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,7 +15,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        EditarDireccion.OnFragmentInteractionListener,
+        EditarTelefonosTarjeta.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,5 +103,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
